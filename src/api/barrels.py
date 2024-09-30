37353299d96ae7309ec881db8d518aefca216406
katieslobodsky <katieslobodsky@gmail.com>
2024-09-29 17:36:27 -0700

    update post_deliver_barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -23,6 +23,16 @@ class Barrel(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
+    # Updating gold & ml
+    with sqlalchemy.Engine.begin() as connection:
+        current_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
+
+    if(current_gold > barrels_delivered[0].price) :
+        with sqlalchemy.Engine.begin() as connection:
+            connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold-{barrels_delivered[0]}"))
+        with sqlalchemy.Engine.begin() as connection:
+            connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = num_green_ml-{barrels_delivered[0].quantity*500}"))
+
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
     return "OK"
