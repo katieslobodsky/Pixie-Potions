13e3620cd02beb9a37ee7c77e8a58ebcc15c90ee
katieslobodsky <katieslobodsky@gmail.com>
2024-10-03 11:09:22 -0700

    update

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,6 @@
+import sqlalchemy
+from src import database as db
+
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
@@ -14,5 +17,7 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = 100, num_green_potions = 0, num_red_potions = 0, num_blue_potions = 0"))
     return "OK"
 
--- src/api/barrels.py
+++ src/api/barrels.py
@@ -24,20 +24,35 @@ class Barrel(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 
-    total_price = barrels_delivered[0].price * barrels_delivered[0].quantity
-    print(f"total price = {total_price}")
+    total_price = 0
+    for barrel in barrels_delivered:
+        if barrel.potion_type == [0, 100, 0, 0]:
+            total_price += barrel.price * barrel.quantity
+        elif barrel.potion_type == [100, 0, 0, 0]:
+            total_price += barrel.price * barrel.quantity 
+        elif barrel.potion_type == [0, 0, 100, 0]:
+            total_price += barrel.price * barrel.quantity 
+
+    print(f"Total price = {total_price}")
 
     with db.engine.begin() as connection:
         current_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
 
         if current_gold < total_price:
             return {"Not enough gold."}
+        
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {total_price}"))
 
-        if current_gold >= total_price:
-            connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {total_price}"))
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :ml"), 
-            {"ml": barrels_delivered[0].quantity * barrels_delivered[0].ml_per_barrel})
-
+        for barrel in barrels_delivered:
+            if barrels_delivered[0].potion_type == [0, 100, 0, 0]:
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :ml"), 
+                                   {"ml": barrels_delivered[0].quantity * barrels_delivered[0].ml_per_barrel})
+            elif barrels_delivered[0].potion_type == [100, 0, 0, 0]:
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml + :ml"), 
+                                   {"ml": barrels_delivered[0].quantity * barrels_delivered[0].ml_per_barrel})
+            elif barrels_delivered[0].potion_type == [0, 0, 100, 0]:
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = num_blue_ml + :ml"), 
+                                   {"ml": barrels_delivered[0].quantity * barrels_delivered[0].ml_per_barrel})
     print(f"Barrels delivered: {barrels_delivered}, order_id: {order_id}")
     return "OK"
 
@@ -46,18 +61,34 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
+    purchase_plan = []
+
     with db.engine.begin() as connection:
         current_green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
+        current_red_potions = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar()
+        current_blue_potions = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar()
 
-        if(current_green_potions < 10) :
-            return [
-                {
+        if current_green_potions < 10:
+            purchase_plan.append({
                 "sku": "SMALL_GREEN_BARREL",
                 "quantity": 1,
-                }
-            ]
-    
+            })
+
+        if current_red_potions < 10:
+            purchase_plan.append({
+                "sku": "SMALL_RED_BARREL",
+                "quantity": 1,
+            })
+
+        if current_blue_potions < 10:
+            purchase_plan.append({
+                "sku": "SMALL_BLUE_BARREL",
+                "quantity": 1,
+            })
+
     print(wholesale_catalog)
 
-    return []
+    return purchase_plan
+
+
 
